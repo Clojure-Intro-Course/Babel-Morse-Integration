24462c52e45459c1311772721d56bb9df48762f8
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2025-04-22 17:23:12 -0500

    Add blank Clojure project shell.
    Add a new leiningen project from the CLI tools.

---
+++ babel-morse-integration/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to babel-morse-integration
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ babel-morse-integration/test/babel_morse_integration/core_test.clj
@@ -0,0 +1,7 @@
+(ns babel-morse-integration.core-test
+  (:require [clojure.test :refer :all]
+            [babel-morse-integration.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
